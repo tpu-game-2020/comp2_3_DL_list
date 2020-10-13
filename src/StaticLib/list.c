edb455d0aaa3570d37cf64bf4d57884b1246d08b
TKSK1848 <sakakitae1848@gmail.com>
2020-10-13 12:27:21 +0900

    テストを通るようにしました。

--- src/StaticLib/list.c
+++ src/StaticLib/list.c
@@ -4,9 +4,31 @@
 //
 // 先頭にdataの値を持つノードを追加する
 //
+
 void add_front(list* l, int data)
 {
 	// ToDo: headにdataの値を持つノードを追加する
+	node* p = (node*)malloc(sizeof(node));
+	if (p == NULL)
+	{
+		return;
+	}
+
+	p->data = data;
+	p->next = l->head;
+	p->prev = NULL;
+
+	l->head = p;
+
+	if (p->next != NULL)
+	{
+		p->next->prev = p;
+	}
+	else
+	{
+		l->tail = p;
+	}
+
 }
 
 //
@@ -15,6 +37,26 @@ void add_front(list* l, int data)
 void add_back(list* l, int data)
 {
 	// ToDo: tailにdataの値を持つノードを追加する
+	node* p = (node*)malloc(sizeof(node));
+	if (p == NULL)
+	{
+		return;
+	}
+
+	p->data = data;
+	p->next = NULL;
+	p->prev = l->tail;
+
+	l->tail = p;
+
+	if (p->prev != NULL)
+	{
+		p->prev->next = p;
+	}
+	else
+	{
+		l->head = p;
+	}
 }
 
 //
@@ -23,7 +65,7 @@ void add_back(list* l, int data)
 node* get_next(node* p)
 {
 	// ToDo: 次の要素を返してください
-	return NULL;
+	return p->next;
 }
 
 //
@@ -32,7 +74,7 @@ node* get_next(node* p)
 node* get_prev(node* p)
 {
 	// ToDo: 前の要素を返してください
-	return NULL;
+	return p->prev;
 }
 
 //
@@ -42,15 +84,45 @@ node* depend(node* p, list* l)
 {
 	// ToDo: pをリストから取り外す
 	// 返り値は次の要素
-	return NULL;
+	node* next = p->next;
+
+	if (p->prev != NULL)
+	{
+		p->prev->next = p->next;
+	}
+	else
+	{
+		l->head = p->next;
+	}
+
+	if (p->next != NULL)
+	{
+		p->next->prev = p->prev;
+	}
+	else
+	{
+		l->tail = p->prev;
+	}
+
+	free(p);
+
+	return next;
 }
 
 //
 // 使用メモリの全解放
 //
-void delete_all(list* p)
+void delete_all(list* l)
 {
 	// ToDo: pで始まるリストのメモリを解放してください
+	node* p = l->head;
+	while (p != NULL)
+	{
+		node* n = p->next;
+		free(p);
+		p = n;
+	}
+
 }
 
 //
@@ -60,4 +132,3 @@ void initialize_list(list* l)
 {
 	l->head = l->tail = NULL;
 }
-
