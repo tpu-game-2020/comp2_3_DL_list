1415ce76fa4a08163196bf97f53bd4eb84712fb2
isigurohatune <39hatune.1925@gmail.com>
2020-10-16 20:17:39 +0900

    テストを通るようにしました。

--- src/StaticLib/list.c
+++ src/StaticLib/list.c
@@ -6,6 +6,22 @@
 //
 void add_front(list* l, int data)
 {
+	node* p = (node*)malloc(sizeof(node));
+	if (p == NULL)return;
+
+	p->data = data;
+	p->next = l->head;
+	p->prev = NULL;
+
+	l->head = p;
+	if (p->next != NULL) {
+		p->next->prev = p;
+	}
+	else {
+		l->tail = p;
+	}
+
+	p->prev = p;
 	// ToDo: headにdataの値を持つノードを追加する
 }
 
@@ -14,6 +30,20 @@ void add_front(list* l, int data)
 //
 void add_back(list* l, int data)
 {
+	node* p = (node*)malloc(sizeof(node));
+	if (p == NULL)return;
+
+	p->data = data;
+	p->next = NULL;
+	p->prev = l->tail;
+
+	l->tail = p;
+	if (p->prev != NULL) {
+		p->prev->next=p;
+	}
+	else {
+		l->head=p;
+	}
 	// ToDo: tailにdataの値を持つノードを追加する
 }
 
@@ -22,8 +52,9 @@ void add_back(list* l, int data)
 //
 node* get_next(node* p)
 {
+
 	// ToDo: 次の要素を返してください
-	return NULL;
+	return p->next;
 }
 
 //
@@ -32,7 +63,7 @@ node* get_next(node* p)
 node* get_prev(node* p)
 {
 	// ToDo: 前の要素を返してください
-	return NULL;
+	return p->prev;
 }
 
 //
@@ -40,16 +71,38 @@ node* get_prev(node* p)
 //
 node* depend(node* p, list* l)
 {
+	node* seednext = p->next;
+
+	if (p->prev!= NULL) {
+		p->prev ->next= p->next;
+	}
+	else {
+		l->head = p->next;
+	}
+	if (p->next!=NULL) {
+		p->next->prev = p->prev;
+	}
+	else {
+		l->tail=p->prev;
+	}
+
+	free (p);
 	// ToDo: pをリストから取り外す
 	// 返り値は次の要素
-	return NULL;
+	return seednext;
 }
 
 //
 // 使用メモリの全解放
 //
-void delete_all(list* p)
+void delete_all(list* l)
 {
+	node* p = l->head;
+	if (p == NULL) {
+		node *n = p->next;
+		free(p);
+		p = n;
+	}
 	// ToDo: pで始まるリストのメモリを解放してください
 }
 
